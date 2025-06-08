922dfc5f46b7d49dfd1361bb5b615c1c0b9c8bc2
Dewranga-Bandara <dewrangabandara071@gmail.com>
2025-06-09 00:38:42 +0530

    initial commit

---
+++ src/test/java/com/cdc/javacapstoneproject/JavaCapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdc.javacapstoneproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCapstoneProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
